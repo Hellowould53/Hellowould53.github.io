9a4ac231b3ef786bf575ef11e2c67823d2c37dcb
Hellowould53 <121003992+Hellowould53@users.noreply.github.com>
2022-12-19 14:56:48 +0900

    Add files via upload

---
+++ space/js/index.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+    var sec02_h = $('#sec02_img_wrap img').height();
+    
+    $('#top').click(function(){
+        $('#sec02_img_wrap').animate({
+            top : -sec02_h,
+
+        },1000,function(){
+            $('#sec02_img_wrap').css({
+                top : 0,
+            }).find('img').first().appendTo('#sec02_img_wrap');
+        });
+    });
+
+
+
+    
+    function time(){
+
+        si = setInterval(function(){
+            $('#top').trigger('click');
+        },3000);
+
+    }
+
+    time();
+
+  
+
+
+
+
+    var a = 0;
+
+    $('#ham_header').hover(function(){
+
+        if( a == 0 ){
+            $('#ham_header img').attr({
+                src : 'img/ham_hover.png',
+            });
+            a++;
+        }else if( a == 1 ){
+            $('img',this).attr({
+                src : 'img/ham.png',
+            });
+            a--;
+        }
+    });
+
+
+
+
+
+});//end
